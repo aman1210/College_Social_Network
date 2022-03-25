6bd93e079424a27d5e723ae54c4660d2f208391b
Aman Srivastava <aman.srivastava101@gmail.com>
2022-03-25 13:32:48 +0530

    structure setup

---
+++ Frontend/college_social_network/lib/utils/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var lightTheme = ThemeData(
+    primaryColor: Colors.blue,
+    brightness: Brightness.light,
+    textTheme: GoogleFonts.poppinsTextTheme());
+
+var darkTheme = ThemeData(
+  primaryColor: Colors.blue,
+  brightness: Brightness.dark,
+  textTheme: GoogleFonts.poppinsTextTheme(),
+);
