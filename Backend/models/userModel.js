fb464401760c155e861b43c264bd01917db28141
Mayank Pratap Singh <itsmayankhere24@gmail.com>
2022-05-19 22:07:40 +0530

    Model for post and user

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,9 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const {Schema} = mongoose;
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    name:{type:String, required:true},
+    email:{type:String, required:true},
+    password:{type:String, required:true},
+    intro:{type:String},
+    about:{type:String},
+    dob:{type:String},
+    location:{type:String, required:true},
+    social_links:[{type:String}],
+    profile_image:{type:String},
+    post:[{type:String}],
+    friendList:[{type:String}]
+})
 
-});
-
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model("User", userSchema);
