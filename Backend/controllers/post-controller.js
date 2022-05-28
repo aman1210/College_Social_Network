d082815b9ae6a10252f937a6ac14ff93f23e6d76
Khushboo Arora <arorakhushboo2558@gmail.com>
2022-05-28 15:07:15 +0530

    post route implemented and deployed

---
+++ Backend/controllers/post-controller.js
@@ -0,0 +1,100 @@
+const Post = require("../models/postModel");
+const Comment = require("../models/commentModel");
+const fs = require("fs");
+
+exports.posts_add_post = (req, res, next) => {
+  var imageslist = [];
+  if (req.files != null) {
+    for (var i = 0; i < req.files.length; i++) {
+      imageslist.push(req.files[i].destination + req.files[i].filename);
+    }
+  }
+  const post = new Post({
+    text: req.body.text,
+    images: imageslist,
+    timeStamp: req.body.timeStamp,
+    userName: req.body.userName,
+  });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message:
+          "Post created successfully! It will appear in feed after verification",
+        post: result,
+      });
+    })
+    .catch((error) => {
+      res
+        .status(error.status || 500)
+        .json({ message: "Something went wrong", error: error });
+    });
+};
+
+exports.posts_get_user_posts = (req, res, next) => {
+  Post.find({ verified: false })
+    .populate({
+      path: "comments",
+      options: {
+        limit: 2,
+        sort: { timeStamp: -1 },
+        // skip: req.params.pageIndex * 2,
+      },
+    })
+    .then((posts) => {
+      const response = {
+        message: "fetched successfully",
+        count: posts.length,
+        posts: posts,
+      };
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(error.status || 500).json({
+        message: " fetching unsuccessful",
+        error: error,
+      });
+    });
+};
+
+exports.posts_add_comment = (req, res, next) => {
+  const newcomment = new Comment({
+    userName: req.body.userName,
+    text: req.body.text,
+    timeStamp: req.body.time,
+  });
+  newcomment.save();
+  Post.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { comments: newcomment } }
+  )
+
+    .then((post) => {
+      console.log(newcomment);
+      res.status(200).json({
+        message: "comment added",
+        post: post,
+      });
+    })
+    .catch((err) => {
+      res.status(err.status || 404).json({
+        message: "Post not found!",
+        error: err,
+      });
+    });
+};
+
+exports.posts_get_post = (req, res, next) => {
+  Post.findById({ _id: req.params.id })
+    .populate("comments")
+    .exec()
+    .then((post) => {
+      res.status(201).json({ post: post });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        message: "Post not found!",
+        error: err,
+      });
+    });
+};
---
+++ Backend/routes/postRoute.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const multer = require("multer");
+
+const postController = require("../controllers/post-controller");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 2,
+  },
+  fileFilter: fileFilter,
+});
+
+const router = express.Router();
+
+router.get("/", postController.posts_get_user_posts);
+
+router.get("/:id", postController.posts_get_post);
+
+router.post("/", upload.array("images", 5), postController.posts_add_post);
+
+router.post("/:id/comment", postController.posts_add_comment);
+
+module.exports = router;
