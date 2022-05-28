0c37423be62a2e58834735a28e11cdda63c3b3d1
Aman Srivastava <aman.srivastava101@gmail.com>
2022-05-28 08:30:46 +0530

    setting up post route

--- Backend/app.js
+++ Backend/app.js
@@ -1,57 +1,77 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoute');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoute");
 
-const session = require('express-session');
-dotenv.config()
+const session = require("express-session");
+dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-const dbURI = "mongodb+srv://connectus:"+process.env.DATABASE_PASSWORD+"@connectus.u2hwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbURI =
+  "mongodb+srv://connectus:" +
+  process.env.DATABASE_PASSWORD +
+  "@connectus.u2hwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, PATCH, DELETE');
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, PATCH, DELETE");
     return res.status(200).json({});
   }
   return next();
 });
 
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'SECRET' 
-}));
-
-app.use("/auth",authRoutes);
-
-
 mongoose
-  .connect(
-    dbURI,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Failed to connect to MongoDB', err));
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
 
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "SECRET",
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
-app.get('/',(req,res)=>{
-    res.send("Hello World!");
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//Handling error likes invalid pages
+app.use((req, res, next) => {
+  let err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//Handling error in database
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: err.message,
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}..`));
---
+++ Backend/routes/postRoute.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb("null", "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb("null", Date.now() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: stroage,
+  limits: {
+    fileSize: 1024 * 1024 * 2,
+  },
+  fileFilter: fileFilter,
+});
+
+const router = express.Router();
+
+module.exports = router;
