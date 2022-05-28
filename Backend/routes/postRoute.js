accfd22dec0d273f385e276d24c760cf6aa95d82
Khushboo Arora <arorakhushboo2558@gmail.com>
2022-05-28 15:32:52 +0530

    setting up cloudinary

--- Backend/routes/postRoute.js
+++ Backend/routes/postRoute.js
@@ -1,36 +1,7 @@
 const express = require("express");
-const multer = require("multer");
+const upload = require("../controllers/multer");
 
 const postController = require("../controllers/post-controller");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/png"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 2,
-  },
-  fileFilter: fileFilter,
-});
 
 const router = express.Router();
 
