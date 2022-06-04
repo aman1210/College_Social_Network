f62e1aa1a682f86b53fc83264e186a3e4bbe6f4d
Aman Srivastava <65115379+aman1210@users.noreply.github.com>
2022-06-04 07:19:48 +0530

    removing error

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -1,31 +1,31 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
 
-exports.user_show_friends = async(req, res, next)=>{
-    console.log(req.params.uid);
-    // return next();
-    
-    const curUser = User.findById({_id:req.params.id});
-    console.log(curUser);
-    User.find({ "friendList.0": { "$exists": true } },function(err,docs) {
-        console.log(docs);
-        docs.
-    })
-    return;
-    // User.find({ verified: true })
-    // curUser.friendList.exec()
-    // .then((friends) => {
-    //   const response = {
-    //     message: "fetched successfully",
-    //     count: friends.length,
-    //     friends: friends,
-    //   };
-    //   res.status(200).json(response);
-    // })
-    // .catch((error) => {
-    //   res.status(error.status || 500).json({
-    //     message: " fetching unsuccessful",
-    //     error: error,
-    //   });
-    // });
-    return;
-}
+exports.user_show_friends = async (req, res, next) => {
+  console.log(req.params.uid);
+  // return next();
+
+  const curUser = User.findById({ _id: req.params.id });
+  console.log(curUser);
+  User.find({ "friendList.0": { $exists: true } }, function (err, docs) {
+    console.log(docs);
+    // docs.
+  });
+  return;
+  // User.find({ verified: true })
+  // curUser.friendList.exec()
+  // .then((friends) => {
+  //   const response = {
+  //     message: "fetched successfully",
+  //     count: friends.length,
+  //     friends: friends,
+  //   };
+  //   res.status(200).json(response);
+  // })
+  // .catch((error) => {
+  //   res.status(error.status || 500).json({
+  //     message: " fetching unsuccessful",
+  //     error: error,
+  //   });
+  // });
+  return;
+};
