92a2290fb5793736fd1e26caaeebb092b4fe68e5
Aman Srivastava <65115379+aman1210@users.noreply.github.com>
2022-06-05 20:33:45 +0530

    fixing

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,12 +1,11 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const signup = async (req, res, next) => {
-
   const { name, email, password, profile_image } = req.body;
   // console.log(req.body);
   let verified = true;
@@ -14,63 +13,77 @@ const signup = async (req, res, next) => {
   try {
     existingUser = await User.findOne({ email });
   } catch (err) {
-    res.status(500).json({ error: "Something went wrong"});
+    res.status(500).json({ error: "Something went wrong" });
     return;
   }
 
   if (existingUser) {
-    res.status(422).json({ error: "User already exists, Please login instead !!"});
+    res
+      .status(422)
+      .json({ error: "User already exists, Please login instead !!" });
     return;
   }
   let hashedPassword;
   try {
     hashedPassword = await bcrypt.hash(password, 12);
   } catch (err) {
-    res.status(500).json({ error: "Could not create user, Please try again !!"});
+    res
+      .status(500)
+      .json({ error: "Could not create user, Please try again !!" });
     return;
   }
-  if(!(/@knit.ac.in/.test(email))){
-    verified=false;
+  if (!/@knit.ac.in/.test(email)) {
+    verified = false;
   }
   const createdUser = new User({
     name,
     email,
     password: hashedPassword,
-    intro:"",
-    about:"",
-    dob:"",
-    location:"",
-    social_links:[],
+    intro: "",
+    about: "",
+    dob: "",
+    location: "",
+    social_links: [],
     profile_image,
     verified,
-    post:[],
-    friendList:[],
-    friendRequest:[]
+    post: [],
+    friendList: [],
+    friendRequest: [],
   });
 
   try {
     await createdUser.save();
   } catch (err) {
-    res.status(500).json({ error: "Signup failed, Please try again !!"});
+    res.status(500).json({ error: "Signup failed, Please try again !!" });
     return;
   }
 
   // toObject converts mongoose Object to default js object
 
-
   let token;
   try {
     token = jwt.sign(
-      { userId: createdUser.id, email: createdUser.email, profile_image: createdUser.profile_image, name: createdUser.name },
+      {
+        userId: createdUser.id,
+        email: createdUser.email,
+        profile_image: createdUser.profile_image,
+        name: createdUser.name,
+      },
       process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   } catch (err) {
-    res.status(500).json({ error: "Signup failed, Please try again !!"});
+    res.status(500).json({ error: "Signup failed, Please try again !!" });
     return;
   }
 
-  res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token });
+  res.status(201).json({
+    userId: createdUser.id,
+    email: createdUser.email,
+    token: token,
+    profile_image: createdUser.profile_image,
+    userName: createdUser.userName,
+  });
 };
 
 const login = async (req, res, next) => {
@@ -79,11 +92,13 @@ const login = async (req, res, next) => {
   try {
     existingUser = await User.findOne({ email });
   } catch (err) {
-    return res.status(500).json({ error: "Login failed, Please try again !!"});
+    return res.status(500).json({ error: "Login failed, Please try again !!" });
   }
 
   if (!existingUser) {
-    res.status(401).json({ error: "Could not identify user, Credentials seems wrong"});
+    res
+      .status(401)
+      .json({ error: "Could not identify user, Credentials seems wrong" });
     return;
   }
 
@@ -91,31 +106,42 @@ const login = async (req, res, next) => {
   try {
     isValidPassword = await bcrypt.compare(password, existingUser.password);
   } catch (err) {
-    res.status(500).json({ error: "Could not log you in, please check your credentials"});
+    res
+      .status(500)
+      .json({ error: "Could not log you in, please check your credentials" });
 
     return;
   }
 
   if (!isValidPassword) {
-    res.status(403).json({ error: "Invalid Credentials, could not log you in."});
+    res
+      .status(403)
+      .json({ error: "Invalid Credentials, could not log you in." });
     return;
   }
 
   let token;
   try {
     token = jwt.sign(
-      { userId: existingUser.id, email: existingUser.email, profile_image: existingUser.profile_image, name: existingUser.name },
+      {
+        userId: existingUser.id,
+        email: existingUser.email,
+        profile_image: existingUser.profile_image,
+        name: existingUser.name,
+      },
       process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   } catch (err) {
-      res.status(500).json({ error: "Login failed, Please try again !!"});
-      return;
-    }
+    res.status(500).json({ error: "Login failed, Please try again !!" });
+    return;
+  }
 
   res.json({
     userId: existingUser.id,
     email: existingUser.email,
+    profile_image: existingUser.profile_image,
+    userName: existingUser.userName,
     token: token,
   });
 };
