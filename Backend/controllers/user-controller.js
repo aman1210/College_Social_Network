cce86781bf3d2a52fd9a15a3305651eeaebd38d3
Khushboo Arora <73962179+khushboo0801@users.noreply.github.com>
2022-06-06 22:51:24 +0530

    fixing friendList

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-const {validationResult} = require('express-validator');
+const { validationResult } = require("express-validator");
 
 exports.user_show_friends = async (req, res, next) => {
   let curUser;
@@ -16,7 +16,11 @@ exports.user_show_friends = async (req, res, next) => {
   }
   return res.json({
     friendList: curUser.friendList.map((friend) => {
-      return { name: friend.name, profile_image: friend.profile_image };
+      return {
+        name: friend.name,
+        profile_image: friend.profile_image,
+        _id: friend._id,
+      };
     }),
   });
 };
@@ -39,25 +43,31 @@ exports.user_send_request = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({"error":"Invalid input passes, please check your data again."})
+    return res
+      .status(422)
+      .json({ error: "Invalid input passes, please check your data again." });
   }
   const userId = req.params.uid;
-  User.findByIdAndUpdate(userId, {$addToSet:{friendRequest:req.userData.userId}},function(err,result){
-    if(err){
-      res.status(404).json({"error":"something went wrong!!"});
+  User.findByIdAndUpdate(
+    userId,
+    { $addToSet: { friendRequest: req.userData.userId } },
+    function (err, result) {
+      if (err) {
+        res.status(404).json({ error: "something went wrong!!" });
+      } else {
+        res.json(result);
+      }
     }
-    else
-    {
-      res.json(result);
-    }
-  })
+  );
 };
 
 exports.user_accept_request = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({"error":"Invalid input passes, please check your data again."})
+    return res
+      .status(422)
+      .json({ error: "Invalid input passes, please check your data again." });
   }
   const userId = req.params.uid;
   // User.findByIdAndUpdate(userId, {$pop:{friendRequest:req.userData.userId}},function(err,result){
@@ -69,13 +79,15 @@ exports.user_accept_request = async (req, res, next) => {
   //     return res.json(result);
   //   }
   // })
-  User.findByIdAndUpdate(userId, {$addToSet:{friendList:userId}},function(err,result){
-    if(err){
-      return res.status(404).json({"error":"something went wrong!!"});
+  User.findByIdAndUpdate(
+    userId,
+    { $addToSet: { friendList: userId } },
+    function (err, result) {
+      if (err) {
+        return res.status(404).json({ error: "something went wrong!!" });
+      } else {
+        return res.json(result);
+      }
     }
-    else
-    {
-      return res.json(result);
-    }
-  })
+  );
 };
