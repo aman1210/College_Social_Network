b321976201e06ad9b41df65a3bac6925b81a801c
Mayank Pratap Singh <itsmayankhere24@gmail.com>
2022-06-07 11:35:56 +0530

    Fixing

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -7,7 +7,7 @@ const router=express.Router();
 router.use(checkAuth);
 router.get("/myCommunity/", userController.user_show_friends);
 router.get("/profile/:uid", userController.user_profile);
-router.patch("editProfile/:uid", userController.user_edit_profile);
+router.patch("/editProfile/:uid", userController.user_edit_profile);
 router.get("/friendRequests/", userController.user_show_friendRequests);
 router.patch("/sendRequest/:uid", userController.user_send_request);
 router.patch("/acceptRequest/:uid", userController.user_accept_request);
